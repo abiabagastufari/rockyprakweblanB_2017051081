d0218471180e914edd1763359593572b00560268
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-09-14 04:34:02 +0700

    first commit
    Signed-off-by: abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>

---
+++ app/Database/Seeds/Mahasiswa.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\i18n\Time;
+
+class Mahasiswa extends Seeder
+{
+    public function run()
+    {
+        $data_mahasiswa = [[
+            'npm' => '2017051007',
+            'nama'    => 'Muhammad Faiz',
+            'alamat' => 'Lampung',
+            'created_at' =>Time::now()
+        ],
+        [
+            'npm' => '2017051060',
+            'nama'    => 'Ahmad Al Farizi',
+            'alamat' => 'Depok',
+            'created_at' =>Time::now()
+        ],
+        [
+            'npm' => '2057051019',
+            'nama'    => 'Muhammad Sultan Raisyah',
+            'alamat' => 'Jakarta Selatan',
+            'created_at' =>Time::now()
+        ],
+        [
+            'npm' => '2057051002',
+            'nama'    => 'M Thoriq Al Fajri',
+            'alamat' => 'Jambi',
+            'created_at' =>Time::now()
+        ],
+    ];
+        foreach($data_mahasiswa as $data){
+            // Using Query Builder
+            $this->db->table('mahasiswa')->insert($data);
+        }
+    }
+}
